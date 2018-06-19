9a127701c0403f2947d543962dec15b06c8ff0df
Datguyovrder <rhui@hawk.iit.edu>
2018-06-19 15:27:32 -0500

    trip planner

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ mapboxgl.accessToken = "pk.eyJ1Ijoicmh1aTIxIiwiYSI6ImNqaW00MjRldTAwNTQzd3J0NDh3c
 
 const map = new mapboxgl.Map({
   container: "map",
-  center: [-87.6354, 41.8885], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  center: [41.852396, -87.634534], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
   zoom: 12, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
 
-const DOMElem = document.createElement("div");
-
-
-new mapboxgl.Marker(DOMElem).setLngLat([-87.6354, 41.8885]).addTo(map);
+const markerDomEl = document.createElement("div");
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+new mapboxgl.Marker(markerDomEl).setLngLat([41.852396, -87.634534]).addTo(map);
